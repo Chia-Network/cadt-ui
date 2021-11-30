b4b000e348999c460e937956152f2e8f31362f44
Michael.Taylor <mtaylor@michaeltaylor3d.com>
2021-11-30 17:43:35 -0500

    feat: add cobenefit redux store

--- src/store/actions/climateWarehouseActions.js
+++ src/store/actions/climateWarehouseActions.js
@@ -1,3 +1,38 @@
+import _ from 'lodash';
 import { keyMirror } from '../store-functions';
+import constants from '../../constants';
 
-export const actions = keyMirror();
+import {
+  activateProgressIndicator,
+  deactivateProgressIndicator,
+  setGlobalErrorMessage,
+} from './app';
+
+export const actions = keyMirror('GET_COBENEFITS');
+
+export const getCoBenefits = ({ useMockedResponse = false }) => {
+  return async dispatch => {
+    try {
+      dispatch(activateProgressIndicator);
+
+      let url = `${constants.API_HOST}/co-benefits`;
+      if (useMockedResponse) {
+        url = `${url}/useMock=true`;
+      }
+
+      const response = fetch(url);
+
+      if (response.ok) {
+        const results = await response.json();
+        dispatch({
+          type: actions.GET_COBENEFITS,
+          payload: results,
+        });
+      }
+    } catch {
+      dispatch(setGlobalErrorMessage('Something went wrong...'));
+    } finally {
+      dispatch(deactivateProgressIndicator);
+    }
+  };
+};
--- src/store/reducers/climateWarehouseReducer.js
+++ src/store/reducers/climateWarehouseReducer.js
@@ -1,7 +1,16 @@
-const initialState = {};
+import u from 'updeep';
+
+import { actions as climateWarehouseActions } from '../actions/climateWarehouseActions';
+
+const initialState = {
+  coBenefits: null,
+};
 
 const climateWarehouseReducer = (state = initialState, action) => {
   switch (action.type) {
+    case climateWarehouseActions.GET_COBENEFITS:
+      return u({ coBenefits: action.payload }, state);
+
     default:
       return state;
   }
