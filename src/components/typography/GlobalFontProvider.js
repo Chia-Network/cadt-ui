5d90ddaa3e62d0fc08794267e4578fc08471bffc
Michael.Taylor <mtaylor@michaeltaylor3d.com>
2021-12-06 09:56:25 -0500

    feat: install nunito font and create globalfontprovider component

---
+++ src/components/typography/GlobalFontProvider.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+import NunitoSansRegular from '../../assets/fonts/NunitoSans-Regular.ttf';
+
+const GlobalFontProvider = createGlobalStyle`
+    @font-face {
+        font-family: 'Nunito';
+        src: url(${NunitoSansRegular}) format('truetype');
+        font-weight: 300;
+        font-style: normal;
+        font-display: auto;
+    }
+`;
+
+export { GlobalFontProvider };
