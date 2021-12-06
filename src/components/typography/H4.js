0f56cd5f697f96e587d9cece96e1b0498d3ec4cb
Daniel <daniel.stefan@equilobe.com>
2021-12-06 17:08:41 +0200

    feat: Set up H4 component #29

---
+++ src/components/typography/H4.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled, { withTheme } from 'styled-components';
+
+const Text = withTheme(styled('h4')`
+  color: ${props => props.color || '#00000'};
+  font-size: 1.3125rem;
+  font-family: ${props => props.theme.typography.primary};
+  font-weight: 600;
+  line-height: 1.9688rem;
+  letter-spacing: -0.0275rem;
+`);
+
+const H4 = withTheme(({ children, color }) => {
+  const appStore = useSelector(state => state.app);
+  return (
+    <Text color={color} selectedTheme={appStore.theme}>
+      {children}
+    </Text>
+  );
+});
+
+export { H4 };
