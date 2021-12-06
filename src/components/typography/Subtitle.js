c2fdb8d26da4f44ef629538844aa9f706cf27a23
Stanley Pageot <stanley@rxdu.io>
2021-12-06 07:48:38 -0800

    Feat: Subtitle Component

---
+++ src/components/typography/Subtitle.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled, { withTheme } from 'styled-components';
+
+const Text = withTheme(styled('h1')`
+  color: ${props =>
+    props.color || '#000000'};
+  font-size: 1.5rem;
+  font-family: ${props => props.theme.typography.primary};
+  font-style: normal;
+  font-weight: 300;
+  line-height: 150%;
+  letter-spacing: -0.439024px;
+`);
+
+const Subtitle = withTheme(({ children, color }) => {
+  const appStore = useSelector(state => state.app);
+  return (
+    <Text color={color} selectedTheme={appStore.theme}>
+      {children}
+    </Text>
+  );
+});
+
+export { Subtitle };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Card, H3, H2, H1 } from '../../components';
+import { Subtitle } from '../../components/typography/Subtitle';
 
 const Home = () => {
 
@@ -10,6 +11,7 @@ const Home = () => {
         <H1>This is an H1</H1>
         <H2>This is an H2</H2>
         <H3>This is an H3</H3>
+        <Subtitle>This is a Subtitle</Subtitle>
       </Card>
     </>
   );
