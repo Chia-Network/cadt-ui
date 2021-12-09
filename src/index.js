6e61f77330d853696134b1b387d4850efaba4ef8
Daniel <daniel.stefan@equilobe.com>
2021-12-09 19:08:49 +0200

    feat: Setup StandardInput Component #43

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import './index.css';
 
 import App from './App';
 import store from './store';
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
