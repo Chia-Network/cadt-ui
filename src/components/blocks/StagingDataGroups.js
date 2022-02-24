fa682ef43534d9998df36c846abb9844d43e1f9e
Daniel Stefan <daniel.stefan@equilobe.com>
2022-02-24 11:17:59 +0200

    feat: display staging in detailed view

--- src/components/blocks/StagingDataGroups.js
+++ src/components/blocks/StagingDataGroups.js
@@ -4,8 +4,9 @@ import { FormattedMessage, useIntl } from 'react-intl';
 import { convertPascalCaseToSentenceCase } from '../../utils/stringUtils';
 import { getDiff } from '../../utils/objectUtils';
 import { Modal, MinusIcon, Body, ErrorIcon, SuccessIcon } from '..';
-import { modalTypeEnum, TableDrawer } from '.';
+import { modalTypeEnum } from '.';
 import { useWindowSize } from '../hooks/useWindowSize';
+import { DetailedViewStagingModal } from './DetailedViewStagingModal';
 
 const StyledChangeGroup = styled('div')`
   background: #f0f2f5;
@@ -151,91 +152,172 @@ const InvalidChangeCard = ({ onDeleteChangeGroup, title }) => {
   );
 };
 
-const StagingDataGroups = withTheme(({ headings, data, deleteStagingData }) => {
-  const [drawerRecord, setDrawerRecord] = useState(null);
-  const [drawerUpdates, setDrawerUpdates] = useState(null);
-  const [deleteFromStaging, setDeleteFromStaging] = useState(false);
-  const [deleteUUID, setDeleteUUID] = useState();
-  const ref = useRef(null);
-  const [height, setHeight] = useState(0);
-  const windowSize = useWindowSize();
-  const intl = useIntl();
+const StagingDataGroups = withTheme(
+  ({ headings, data, deleteStagingData, modalSizeAndPosition }) => {
+    const [detailedViewData, setDetailedViewData] = useState(null);
+    const [deleteFromStaging, setDeleteFromStaging] = useState(false);
+    const [deleteUUID, setDeleteUUID] = useState();
+    const ref = useRef(null);
+    const [height, setHeight] = useState(0);
+    const windowSize = useWindowSize();
+    const intl = useIntl();
 
-  const changeGroupIsValid = changeGroup => {
-    if (!changeGroup.diff) {
-      return false;
-    }
-
-    if (!changeGroup.diff.original || !changeGroup.diff.change) {
-      return false;
-    }
-
-    if (changeGroup.diff.change.length === 1 && !changeGroup.diff.change[0]) {
-      return false;
-    }
-
-    if (
-      changeGroup.diff.change.length === 2 &&
-      (!changeGroup.diff.change[0] || !changeGroup.diff.change[1])
-    ) {
-      return false;
-    }
-
-    return true;
-  };
-
-  useEffect(() => {
-    setHeight(windowSize.height - ref.current.getBoundingClientRect().top - 20);
-  }, [ref.current, windowSize.height, data]);
-
-  const onDeleteStaging = uuid => {
-    if (!deleteStagingData) return null;
-    return () => {
-      deleteStagingData(uuid);
-      setDeleteFromStaging(false);
-    };
-  };
-
-  const getTranslatedCardTitle = (tableC, actionC, changeArrayLength) => {
-    const table = tableC.toLowerCase();
-    const action = actionC.toLowerCase();
-    let translationId = 'record';
-    if (table === 'projects') {
-      if (action === 'delete') {
-        translationId = 'delete-project';
-      } else if (action === 'update') {
-        translationId = 'update-project';
-      } else if (action === 'insert') {
-        translationId = 'add-project';
+    const changeGroupIsValid = changeGroup => {
+      if (!changeGroup.diff) {
+        return false;
       }
-    } else if (table === 'units') {
-      if (action === 'delete') {
-        translationId = 'delete-unit';
-      } else if (action === 'add') {
-        translationId = 'add-unit';
-      } else if (action === 'update') {
-        if (changeArrayLength && changeArrayLength > 1) {
-          translationId = 'split-unit';
-        } else {
-          translationId = 'update-unit';
+
+      if (!changeGroup.diff.original || !changeGroup.diff.change) {
+        return false;
+      }
+
+      if (changeGroup.diff.change.length === 1 && !changeGroup.diff.change[0]) {
+        return false;
+      }
+
+      if (
+        changeGroup.diff.change.length === 2 &&
+        (!changeGroup.diff.change[0] || !changeGroup.diff.change[1])
+      ) {
+        return false;
+      }
+
+      return true;
+    };
+
+    useEffect(() => {
+      setHeight(
+        windowSize.height - ref.current.getBoundingClientRect().top - 20,
+      );
+    }, [ref.current, windowSize.height, data]);
+
+    const onDeleteStaging = uuid => {
+      if (!deleteStagingData) return null;
+      return () => {
+        deleteStagingData(uuid);
+        setDeleteFromStaging(false);
+      };
+    };
+
+    const getTranslatedCardTitle = changeGroup => {
+      const table = changeGroup.table.toLowerCase();
+      const action = changeGroup.action.toLowerCase();
+      let translationId = 'record';
+
+      if (table === 'projects') {
+        if (action === 'delete') {
+          translationId = 'delete-project';
+        } else if (action === 'update') {
+          translationId = 'update-project';
+        } else if (action === 'insert') {
+          translationId = 'add-project';
+        }
+      } else if (table === 'units') {
+        if (action === 'delete') {
+          translationId = 'delete-unit';
+        } else if (action === 'insert') {
+          translationId = 'add-unit';
+        } else if (action === 'update') {
+          if (
+            changeGroup.diff.change.length &&
+            changeGroup.diff.change.length > 1
+          ) {
+            translationId = 'split-unit';
+          } else {
+            translationId = 'update-unit';
+          }
         }
       }
-    }
-    return intl.formatMessage({
-      id: translationId,
-    });
-  };
+      return intl.formatMessage({
+        id: translationId,
+      });
+    };
 
-  return (
-    <StagingDataGroupsContainer ref={ref}>
-      <div style={{ height: `${height}px`, overflow: 'auto' }}>
-        {data &&
-          headings &&
-          data.map((changeGroup, index) => (
-            <React.Fragment key={index}>
-              {changeGroupIsValid(changeGroup) && (
-                <StyledChangeGroup>
-                  {deleteStagingData && (
+    return (
+      <StagingDataGroupsContainer ref={ref}>
+        <div style={{ height: `${height}px`, overflow: 'auto' }}>
+          {data &&
+            headings &&
+            data.map((changeGroup, index) => (
+              <React.Fragment key={index}>
+                {changeGroupIsValid(changeGroup) && (
+                  <StyledChangeGroup>
+                    {deleteStagingData && (
+                      <StyledDeleteGroupIcon>
+                        <div
+                          onClick={() => {
+                            setDeleteUUID(changeGroup.uuid);
+                            setDeleteFromStaging(true);
+                          }}
+                        >
+                          <MinusIcon width={20} height={20} />
+                        </div>
+                      </StyledDeleteGroupIcon>
+                    )}
+                    {changeGroup.action === 'DELETE' && (
+                      <ChangeCard
+                        data={changeGroup.diff.original}
+                        headings={headings}
+                        onClick={() =>
+                          setDetailedViewData(changeGroup.diff.original)
+                        }
+                        title={getTranslatedCardTitle(changeGroup)}
+                        displayInRed
+                      />
+                    )}
+                    {changeGroup.action === 'INSERT' && (
+                      <ChangeCard
+                        data={changeGroup.diff.change[0]}
+                        headings={headings}
+                        title={getTranslatedCardTitle(changeGroup)}
+                        onClick={() =>
+                          setDetailedViewData({
+                            record: changeGroup.diff.change[0],
+                            title: getTranslatedCardTitle(changeGroup),
+                          })
+                        }
+                      />
+                    )}
+                    {changeGroup.action === 'UPDATE' && (
+                      <ChangeCard
+                        data={changeGroup.diff.original}
+                        headings={getDiff(
+                          changeGroup.diff.original,
+                          changeGroup.diff.change[0],
+                        )}
+                        onClick={() =>
+                          setDetailedViewData({
+                            record: changeGroup.diff.original,
+                            changes: changeGroup.diff.change,
+                            title: getTranslatedCardTitle(changeGroup),
+                          })
+                        }
+                        title={getTranslatedCardTitle(changeGroup)}
+                        deletedIsVsible
+                        displayInRed
+                      />
+                    )}
+                    {changeGroup.action === 'UPDATE' &&
+                      changeGroup.diff.change.map((change, index) => (
+                        <ChangeCard
+                          key={index}
+                          data={change}
+                          headings={getDiff(changeGroup.diff.original, change)}
+                          title={getTranslatedCardTitle(changeGroup)}
+                          onClick={() =>
+                            setDetailedViewData({
+                              record: changeGroup.diff.original,
+                              changes: changeGroup.diff.change,
+                              title: getTranslatedCardTitle(changeGroup),
+                            })
+                          }
+                          addedIsVisible
+                        />
+                      ))}
+                  </StyledChangeGroup>
+                )}
+                {!changeGroupIsValid(changeGroup) && (
+                  <StyledChangeGroup>
                     <StyledDeleteGroupIcon>
                       <div
                         onClick={() => {
@@ -246,126 +328,52 @@ const StagingDataGroups = withTheme(({ headings, data, deleteStagingData }) => {
                         <MinusIcon width={20} height={20} />
                       </div>
                     </StyledDeleteGroupIcon>
-                  )}
-                  {changeGroup.action === 'DELETE' && (
-                    <ChangeCard
-                      data={changeGroup.diff.original}
-                      headings={headings}
-                      onClick={() => setDrawerRecord(changeGroup.diff.original)}
-                      title={getTranslatedCardTitle(
-                        changeGroup.table,
-                        changeGroup.action,
-                      )}
-                      displayInRed
-                    />
-                  )}
-                  {changeGroup.action === 'INSERT' && (
-                    <ChangeCard
-                      data={changeGroup.diff.change[0]}
-                      headings={headings}
-                      title={getTranslatedCardTitle(
-                        changeGroup.table,
-                        changeGroup.action,
-                      )}
-                      onClick={() =>
-                        setDrawerRecord(changeGroup.diff.change[0])
-                      }
-                    />
-                  )}
-                  {changeGroup.action === 'UPDATE' && (
-                    <ChangeCard
-                      data={changeGroup.diff.original}
-                      headings={getDiff(
-                        changeGroup.diff.original,
-                        changeGroup.diff.change[0],
-                      )}
-                      onClick={() => {
-                        setDrawerRecord(changeGroup.diff.original);
-                        setDrawerUpdates(changeGroup.diff.change);
-                      }}
-                      title={getTranslatedCardTitle(
-                        changeGroup.table,
-                        changeGroup.action,
-                        changeGroup.diff.change.length,
-                      )}
-                      deletedIsVsible
-                      displayInRed
-                    />
-                  )}
-                  {changeGroup.action === 'UPDATE' &&
-                    changeGroup.diff.change.map((change, index) => (
-                      <ChangeCard
-                        key={index}
-                        data={change}
-                        headings={getDiff(changeGroup.diff.original, change)}
-                        title={getTranslatedCardTitle(
-                          changeGroup.table,
-                          changeGroup.action,
-                          changeGroup.diff.change.length,
-                        )}
-                        onClick={() => {
-                          setDrawerRecord(changeGroup.diff.original);
-                          setDrawerUpdates(changeGroup.diff.change);
-                        }}
-                        addedIsVisible
-                      />
-                    ))}
-                </StyledChangeGroup>
-              )}
-              {!changeGroupIsValid(changeGroup) && (
-                <StyledChangeGroup>
-                  <StyledDeleteGroupIcon>
-                    <div
-                      onClick={() => {
+                    <InvalidChangeCard
+                      title={intl.formatMessage({
+                        id:
+                          changeGroup.table.toLowerCase() === 'projects'
+                            ? 'project'
+                            : 'unit',
+                      })}
+                      onDeleteChangeGroup={() => {
                         setDeleteUUID(changeGroup.uuid);
                         setDeleteFromStaging(true);
                       }}
-                    >
-                      <MinusIcon width={20} height={20} />
-                    </div>
-                  </StyledDeleteGroupIcon>
-                  <InvalidChangeCard
-                    title={intl.formatMessage({
-                      id:
-                        changeGroup.table.toLowerCase() === 'projects'
-                          ? 'project'
-                          : 'unit',
-                    })}
-                    onDeleteChangeGroup={() => {
-                      setDeleteUUID(changeGroup.uuid);
-                      setDeleteFromStaging(true);
-                    }}
-                  />
-                </StyledChangeGroup>
-              )}
-            </React.Fragment>
-          ))}
-        {drawerRecord && (
-          <TableDrawer
-            drawerRecord={drawerRecord}
-            drawerUpdates={drawerUpdates}
-            onClose={() => {
-              setDrawerRecord(null);
-              setDrawerUpdates(null);
-            }}
-          />
-        )}
-        {deleteFromStaging && (
-          <Modal
-            title={intl.formatMessage({
-              id: 'notification',
-            })}
-            body={intl.formatMessage({
-              id: 'confirm-deletion',
-            })}
-            modalType={modalTypeEnum.confirmation}
-            onClose={() => setDeleteFromStaging(false)}
-            onOk={onDeleteStaging(deleteUUID)}
-          />
-        )}
-      </div>
-    </StagingDataGroupsContainer>
-  );
-});
+                    />
+                  </StyledChangeGroup>
+                )}
+              </React.Fragment>
+            ))}
+          {detailedViewData && (
+            <DetailedViewStagingModal
+              onClose={() => setDetailedViewData(null)}
+              modalSizeAndPosition={modalSizeAndPosition}
+              record={detailedViewData.record}
+              changes={
+                detailedViewData?.changes ? detailedViewData.changes : undefined
+              }
+              title={
+                detailedViewData?.title ? detailedViewData.title : undefined
+              }
+            />
+          )}
+          {deleteFromStaging && (
+            <Modal
+              title={intl.formatMessage({
+                id: 'notification',
+              })}
+              body={intl.formatMessage({
+                id: 'confirm-deletion',
+              })}
+              modalType={modalTypeEnum.confirmation}
+              onClose={() => setDeleteFromStaging(false)}
+              onOk={onDeleteStaging(deleteUUID)}
+            />
+          )}
+        </div>
+      </StagingDataGroupsContainer>
+    );
+  },
+);
 
 export { StagingDataGroups };
