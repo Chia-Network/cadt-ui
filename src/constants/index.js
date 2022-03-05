804b80f75f9c59e393ea9bb9f293b16388d0cbe8
Michael Taylor <mtaylor@michaeltaylor.dev>
2022-03-05 08:16:37 -0500

    chore: revert constant change

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export default {
-  API_HOST: 'http://54.212.115.104:3030/v1',
+  API_HOST: 'http://localhost:3030/v1',
   MAX_TABLE_SIZE: 7,
   HEADER_HEIGHT: 64, // Needed to be used to calculate max height for body components
   THEME: {
