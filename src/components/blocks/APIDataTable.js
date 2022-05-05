61083d6ee563e0dc4feb3e2b068798fce8b3129d
SPageot <stanley@rxdu.io>
2022-05-05 06:37:48 -0700

    Update APIDataTable.js

--- src/components/blocks/APIDataTable.js
+++ src/components/blocks/APIDataTable.js
@@ -230,8 +230,10 @@ const APIDataTable = withTheme(
                             ].toString()}`
                           }
                         >
-                          {key === 'orgUid' && organizations[record[key]] && (
+                            {key === 'orgUid' && organizations[record[key]] && (
                             <img src={organizations[record[key]].icon} />
+                          )}
+                          
                           {key !== 'orgUid' &&
                             record[key] &&
                             record[key] !== 'null' &&
