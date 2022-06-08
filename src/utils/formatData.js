ae1142d0dd2e75e7cf8797a4860e263cb18b4bd1
Daniel Stefan <daniel.stefan@equilobe.com>
2022-06-08 17:13:04 +0300

    feat: save dates as UTC but view as international date format

--- src/utils/formatData.js
+++ src/utils/formatData.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
 import dayjs from 'dayjs';
 
+// function is applied to onChange newValue received from MUI DateSelect so it's compatible with what the API expects
 export const formatDate = date => dayjs(date).format('YYYY-MM-DDT00:00:00[Z]');
 
+// function is applied to the dates the ui receives from the api for detailed or staging view
 export const getISODate = date => {
   const isValidDate = !isNaN(Date.parse(date));
-  return isValidDate ? dayjs(date).format('YYYY/MM/DD') : date;
+  return isValidDate ? dayjs(date, 'YYYY-MM-DD').format('YYYY/MM/DD') : date;
 };
 
 export const formatAPIData = unformattedData => {
