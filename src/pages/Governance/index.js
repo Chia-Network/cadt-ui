b217231cf225f013633fdf83bd28c6d8b8bbe7c7
Daniel Stefan <daniel.stefan@equilobe.com>
2022-06-13 19:50:59 +0300

    feat: build governance UI

--- src/pages/Governance/index.js
+++ src/pages/Governance/index.js
@@ -10,6 +10,9 @@ import {
   TextareaStateEnum,
   PrimaryButton,
   Body,
+  InfoIconSmall,
+  modalTypeEnum,
+  Modal,
 } from '../../components';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -21,6 +24,7 @@ import {
   updateGovernancePickLists,
 } from '../../store/actions/climateWarehouseActions';
 import { isJsonString } from '../../utils/json';
+import constants from '../../constants';
 
 const StyledGovernanceContainer = styled('div')`
   padding: 30px 63px;
@@ -45,15 +49,35 @@ const StyledJSONContainer = styled.div`
   }
 `;
 
+const StyledJSONExampleContainer = styled.div`
+  textarea {
+    height: 50vh;
+    width: 40vw;
+    pointer-events: none;
+  }
+`;
+
 const StyledButtonContainer = styled.div`
   width: fit-content;
 `;
+
+const StyledInfoIconContainer = styled.span`
+  cursor: pointer;
+  padding-left: 8px;
+`;
+
+const modalToOpenEnum = {
+  none: 'none',
+  pickLists: 'pickLists',
+  orgList: 'orgList',
+};
 
 const Governance = () => {
   const intl = useIntl();
   const [picklistsTextarea, setPicklistsTextarea] = useState(
     JSON.stringify({}),
   );
+  const [modalToOpen, setModalToOpen] = useState(modalToOpenEnum.none);
   const [orgListTextarea, setOrgListTextarea] = useState(JSON.stringify([]));
   const {
     pickLists,
@@ -130,89 +154,130 @@ const Governance = () => {
   }, [isGovernanceInitiated, isGovernanceCreated]);
 
   return (
-    <StyledGovernanceContainer>
-      <H2>
-        <FormattedMessage id="governance" />
-      </H2>
-      {isGovernanceCreated && (
-        <StyledJSONSectionContainer>
-          <StyledJSONContainer>
-            <div>
-              <H4>
-                <FormattedMessage id="picklists" />
-              </H4>
-              {arePickListsValid && (
-                <Body size="Small" color="green">
-                  <FormattedMessage id="json-is-valid" />
-                </Body>
-              )}
-              {!arePickListsValid && (
-                <Body size="Small" color="red">
-                  <FormattedMessage id="json-not-valid" />
-                </Body>
-              )}
-            </div>
-            <Textarea
-              size={TextareaSizeEnum.large}
-              placeholder={intl.formatMessage({
-                id: 'picklists',
-              })}
-              value={prettyfiedPicklists}
-              onChange={e => setPicklistsTextarea(e.target.value)}
-              state={TextareaStateEnum.default}
-            />
-          </StyledJSONContainer>
-          <StyledJSONContainer>
-            <div>
-              <H4>
-                <FormattedMessage id="all-organizations" />
-              </H4>
-              {isOrgListValid && (
-                <Body size="Small" color="green">
-                  <FormattedMessage id="json-is-valid" />
-                </Body>
-              )}
-              {!isOrgListValid && (
-                <Body size="Small" color="red">
-                  <FormattedMessage id="json-not-valid" />
-                </Body>
-              )}
-            </div>
-            <Textarea
-              size={TextareaSizeEnum.large}
-              placeholder={intl.formatMessage({
-                id: 'all-organizations',
-              })}
-              value={prettyfiedOrgList}
-              onChange={e => setOrgListTextarea(e.target.value)}
-              state={TextareaStateEnum.default}
-            />
-          </StyledJSONContainer>
-        </StyledJSONSectionContainer>
-      )}
-      <StyledButtonContainer>
-        {!isGovernanceInitiated && !isGovernanceCreated && (
-          <PrimaryButton
-            label={intl.formatMessage({ id: 'initiate-governance' })}
-            size="large"
-            onClick={() => dispatch(initiateGovernance())}
-          />
-        )}
-        {isGovernanceInitiated && !isGovernanceCreated && (
-          <H4>
-            <FormattedMessage id="governance-initiating-please-wait" />
-          </H4>
-        )}
+    <>
+      <StyledGovernanceContainer>
+        <H2>
+          <FormattedMessage id="governance" />
+        </H2>
         {isGovernanceCreated && (
-          <PrimaryButton
-            label={intl.formatMessage({ id: 'send' })}
-            size="large"
-            onClick={handleOnSend}
-            disabled={!arePickListsValid || !isOrgListValid}
-          />
+          <StyledJSONSectionContainer>
+            <StyledJSONContainer>
+              <div>
+                <H4>
+                  <FormattedMessage id="picklists" />
+                  <StyledInfoIconContainer
+                    onClick={() => setModalToOpen(modalToOpenEnum.pickLists)}
+                  >
+                    <InfoIconSmall height="14" width="14" />
+                  </StyledInfoIconContainer>
+                </H4>
+                {arePickListsValid && (
+                  <Body size="Small" color="green">
+                    <FormattedMessage id="json-is-valid" />
+                  </Body>
+                )}
+                {!arePickListsValid && (
+                  <Body size="Small" color="red">
+                    <FormattedMessage id="json-not-valid" />
+                  </Body>
+                )}
+              </div>
+              <Textarea
+                size={TextareaSizeEnum.large}
+                placeholder={intl.formatMessage({
+                  id: 'picklists',
+                })}
+                value={prettyfiedPicklists}
+                onChange={e => setPicklistsTextarea(e.target.value)}
+                state={TextareaStateEnum.default}
+              />
+            </StyledJSONContainer>
+            <StyledJSONContainer>
+              <div>
+                <H4>
+                  <FormattedMessage id="all-organizations" />
+                  <StyledInfoIconContainer
+                    onClick={() => setModalToOpen(modalToOpenEnum.orgList)}
+                  >
+                    <InfoIconSmall height="14" width="14" />
+                  </StyledInfoIconContainer>
+                </H4>
+                {isOrgListValid && (
+                  <Body size="Small" color="green">
+                    <FormattedMessage id="json-is-valid" />
+                  </Body>
+                )}
+                {!isOrgListValid && (
+                  <Body size="Small" color="red">
+                    <FormattedMessage id="json-not-valid" />
+                  </Body>
+                )}
+              </div>
+              <Textarea
+                size={TextareaSizeEnum.large}
+                placeholder={intl.formatMessage({
+                  id: 'all-organizations',
+                })}
+                value={prettyfiedOrgList}
+                onChange={e => setOrgListTextarea(e.target.value)}
+                state={TextareaStateEnum.default}
+              />
+            </StyledJSONContainer>
+          </StyledJSONSectionContainer>
         )}
-      </StyledButtonContainer>
-    </StyledGovernanceContainer>
+        <StyledButtonContainer>
+          {!isGovernanceInitiated && !isGovernanceCreated && (
+            <PrimaryButton
+              label={intl.formatMessage({ id: 'initiate-governance' })}
+              size="large"
+              onClick={() => dispatch(initiateGovernance())}
+            />
+          )}
+          {isGovernanceInitiated && !isGovernanceCreated && (
+            <H4>
+              <FormattedMessage id="governance-initiating-please-wait" />
+            </H4>
+          )}
+          {isGovernanceCreated && (
+            <PrimaryButton
+              label={intl.formatMessage({ id: 'send' })}
+              size="large"
+              onClick={handleOnSend}
+              disabled={!arePickListsValid || !isOrgListValid}
+            />
+          )}
+        </StyledButtonContainer>
+      </StyledGovernanceContainer>
+      {(modalToOpen === modalToOpenEnum.orgList ||
+        modalToOpen === modalToOpenEnum.pickLists) && (
+        <Modal
+          onClose={() => setModalToOpen(modalToOpenEnum.none)}
+          modalType={modalTypeEnum.basic}
+          title={`${intl.formatMessage({
+            id: 'valid-json-example',
+          })} (${intl.formatMessage({
+            id:
+              modalToOpen === modalToOpenEnum.orgList
+                ? 'picklists'
+                : 'all-organizations',
+          })})`}
+          body={
+            <StyledJSONExampleContainer>
+              <Textarea
+                size={TextareaSizeEnum.large}
+                value={
+                  modalToOpen === modalToOpenEnum.orgList
+                    ? constants.ORG_LIST_JSON_EXAMPLE
+                    : constants.PICK_LIST_JSON_EXAMPLE
+                }
+                readonly
+              />
+            </StyledJSONExampleContainer>
+          }
+          hideButtons
+        />
+      )}
+    </>
   );
 };
 
