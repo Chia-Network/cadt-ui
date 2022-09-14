1c442465a11437c90da93f1da37f51550ab0e379
Daniel Stefan <daniel.stefan@equilobe.com>
2022-09-14 19:24:17 +0300

    feat: replace offer with transfer offer

--- src/store/actions/climateWarehouseActions.js
+++ src/store/actions/climateWarehouseActions.js
@@ -1627,7 +1627,7 @@ export const makerDownloadTransferOffer = async () => {
       const link = document.createElement('a');
       const url = window.URL.createObjectURL(new Blob([filename]));
       link.href = url;
-      link.download = `offer.txt`;
+      link.download = `transfer-offer.txt`;
       document.body.appendChild(link); // Required for this to work in FireFox
       link.click();
     });
