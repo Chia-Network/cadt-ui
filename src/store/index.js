86891b7ea05024aebaada24c6d7f7fd0cc2a6044
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-01-31 13:22:02 -0500

    chore: fix import

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 import { appReducer, climateWarehouseReducer } from './reducers';
 
