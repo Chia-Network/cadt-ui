cf211e345c152192917992252b30548f0a5153a7
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-03-22 19:20:16 -0400

    feat: simple scroll bar

--- src/renderer/components/layout/DataTable.tsx
+++ src/renderer/components/layout/DataTable.tsx
@@ -1,5 +1,8 @@
 import { noop } from 'lodash';
 import { FormattedMessage } from 'react-intl';
+import { Tooltip } from 'flowbite-react';
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 
 interface Column {
   key: string;
@@ -26,9 +29,8 @@ const DataTable: React.FC<DataTableProps> = ({
   data,
   isLoading = false,
   onRowClick = noop,
-  footer = null
+  footer = null,
 }) => {
-
   if (isLoading) {
     return null;
   }
@@ -37,62 +39,88 @@ const DataTable: React.FC<DataTableProps> = ({
     <div className="dark:bg-gray-800">
       {/* Mobile view */}
       <div className="block md:hidden mx-auto w-full">
-        {data?.length > 0 && data.map((row) => (
-          <div key={row[primaryKey]} onClick={() => onRowClick(row)} className="mb-4 p-4 border border-gray-200 rounded dark:bg-gray-700">
-            {columns.map((column) => (
-              <div key={`${column.key}-${row[primaryKey]}`} className="py-2">
-                <div className="font-bold text-left text-gray-900 dark:text-gray-100">
-                  {column.renderHeader ? column.renderHeader(column) : column.title}
+        {data?.length > 0 &&
+          data.map((row) => (
+            <div
+              key={row[primaryKey]}
+              onClick={() => onRowClick(row)}
+              className="mb-4 p-4 border border-gray-200 rounded dark:bg-gray-700"
+            >
+              {columns.map((column) => (
+                <div key={`${column.key}-${row[primaryKey]}`} className="py-2">
+                  <div className="font-bold text-left text-gray-900 dark:text-gray-100">
+                    {column.renderHeader ? column.renderHeader(column) : column.title}
+                  </div>
+                  <div className="text-left text-gray-600 dark:text-white">
+                    {column.render ? (
+                      column.render(row)
+                    ) : (
+                      <div className="truncate" style={{ maxWidth: '300px' }}>
+                        {row[column.key]}
+                      </div>
+                    )}
+                  </div>
                 </div>
-                <div className="text-left text-gray-600 dark:text-white">
-                  {column.render ? column.render(row) : (
-                    <div className="truncate" style={{ maxWidth: '300px' }}>
-                      {row[column.key]}
-                    </div>
-                  )}
-                </div>
-              </div>
-            ))}
-          </div>
-        ))}
+              ))}
+            </div>
+          ))}
       </div>
 
       {/* Desktop view */}
-      <div className="hidden md:flex md:flex-col" style={{ height: 'calc(100vh - 150px)', width: 'calc(100vw - 260px)' }}>
-        <div className="overflow-auto">
-          <table className="min-w-full divide-y divide-gray-300 dark:divide-gray-700" style={{height: 'calc(100vh - 265px)'}}>
+
+      <div
+        className="hidden md:flex md:flex-col custom-scrollbar"
+        style={{ height: 'calc(100vh - 150px)', width: 'calc(100vw - 260px)' }}
+      >
+        <SimpleBar style={{ maxHeight: '100%' }} autoHide={false} forceVisible="x">
+          <table
+            className="min-w-full divide-y divide-gray-300 dark:divide-gray-700 custom-scrollbar"
+            style={{ height: data.length > 5 ? 'calc(100vh - 265px)' : 'auto' }}
+          >
             <thead className="bg-gray-50 dark:bg-gray-700 sticky top-0 z-10">
               <tr>
                 {columns.map((column) => (
-                  <th key={column.key} className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
+                  <th
+                    key={column.key}
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider"
+                  >
                     {column.renderHeader ? column.renderHeader(column) : column.title}
                   </th>
                 ))}
               </tr>
             </thead>
             <tbody className="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
-              {data?.length > 0 && data.map((row, index) => (
-                <tr key={row[primaryKey]} onClick={() => onRowClick(row)} className={index % 2 === 0 ? 'bg-gray-50 dark:bg-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700' : 'bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700'}>
-                  {columns.map((column) => (
-                    <td key={`${column.key}-${row[primaryKey]}`} className="px-6 py-4 whitespace-normal">
-                      <div className="text-gray-600 dark:text-white">
-                        {column.render ? column.render(row) : (
-                          <div className="truncate" style={{ maxWidth: '300px' }}>
-                            {row[column.key]}
-                          </div>
-                        )}
-                      </div>
-                    </td>
-                  ))}
-                </tr>
-              ))}
+              {data?.length > 0 &&
+                data.map((row, index) => (
+                  <tr
+                    key={row[primaryKey]}
+                    onClick={() => onRowClick(row)}
+                    className={
+                      index % 2 === 0
+                        ? 'bg-gray-50 dark:bg-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700'
+                        : 'bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700'
+                    }
+                  >
+                    {columns.map((column) => (
+                      <td key={`${column.key}-${row[primaryKey]}`} className="px-6 py-4 whitespace-normal">
+                        <div className="text-gray-600 dark:text-white">
+                          {column.render ? (
+                            column.render(row)
+                          ) : (
+                            <div className="truncate" style={{ maxWidth: '300px' }}>
+                              <Tooltip content={row[column.key]}>{row[column.key]}</Tooltip>
+                            </div>
+                          )}
+                        </div>
+                      </td>
+                    ))}
+                  </tr>
+                ))}
             </tbody>
           </table>
-        </div>
+        </SimpleBar>
         {footer && data.length && (
-          <div className="mt-auto bg-gray-50 dark:bg-gray-700 w-full p-4 text-left">
-            {footer}
-          </div>
+          <div className="mt-auto bg-gray-50 dark:bg-gray-700 w-full p-4 text-left">{footer}</div>
         )}
       </div>
 
