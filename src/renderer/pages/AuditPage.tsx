d647e78b802346b27274e048ab6730fdfc980002
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-05-08 11:10:23 -0400

    fix: add focus to the select organization on the audit table

--- src/renderer/pages/AuditPage.tsx
+++ src/renderer/pages/AuditPage.tsx
@@ -67,15 +67,21 @@ const AuditPage: React.FC = () => {
   if (!orgUid) {
     return (
       <>
-        <div className="flex flex-col md:flex-row gap-6 pl-1 my-2.5 ml-2.5 relative z-30">
-          <OrganizationSelector onSelect={handleOrganizationSelected} defaultOrgUid={orgUid} />
+        <div className="flex flex-col md:flex-row gap-6 px-1 py-2.5 ml-2.5 items-center justify-center min-h-screen bg-gray-800 bg-opacity-50">
+          <div className="p-6 bg-white rounded-lg shadow-xl z-40">
+            <OrganizationSelector onSelect={handleOrganizationSelected} defaultOrgUid={orgUid} />
+          </div>
         </div>
-        <div className="flex h-full justify-center items-center">
-          <FormattedMessage id="please-select-an-organization-to-view-audit-data" />
+        <div className="flex h-screen justify-center items-center bg-gray-800 bg-opacity-75">
+          <div className="text-lg text-white font-medium p-4 bg-gray-900 rounded-md shadow-lg">
+            <FormattedMessage id="please-select-an-organization-to-view-audit-data" />
+          </div>
         </div>
       </>
     );
   }
+  
+  
 
   if (auditLoading) {
     return <SkeletonTable />;
