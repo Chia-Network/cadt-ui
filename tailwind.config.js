00e2968c58d3c6a652787cce0bf6daac5405a283
William Wills <wwills2@protonmail.com>
2024-12-02 17:12:47 -0500

    chore: address flowbite-react 0.8.0+ breaking changes chore: update flowbite-react to 0.10.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+import * as flowbite from 'flowbite-react/tailwind';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', 'node_modules/flowbite-react/lib/esm/**/*.js'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', flowbite.content()],
   theme: {
     extend: {
       colors: {
